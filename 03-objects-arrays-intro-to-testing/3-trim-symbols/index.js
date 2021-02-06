c12547680c0cb974cdb437aa36b21dc4a3142d43
Марина Козлова <ksmarina@Marinas-MacBook-Pro.local>
2021-02-06 17:11:35 +0300

    Third task

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,22 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  let symbolsCounter = {};
+  let newString = '';
+  
+  if (size === 0) return newString;
+  else if (!size) return string;
 
+  for (let symbol of string) {
+    if (symbol in symbolsCounter) {
+      if (symbolsCounter[symbol] >= size) continue;
+      symbolsCounter[symbol] += 1;
+      newString += symbol;
+    } else {
+      symbolsCounter = {};
+      symbolsCounter[symbol] = 1;
+      newString += symbol;
+    }
+  }
+  return newString;
 }
